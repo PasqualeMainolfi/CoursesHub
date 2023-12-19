2213eb5fd4f7a2883e2abf0fefe1d91f05f89bd7
PasqualeMainolfi <mnlpql@gmail.com>
2023-12-19 19:47:39 +0100

    add javaproc

---
+++ LECTURE_NOTES/MSTPL/first_year_bachelor/java_proc/src/Main.java
@@ -0,0 +1,37 @@
+import processing.core.*;
+
+public class Main extends PApplet {
+
+    int n = 100;
+
+    public void setting() {
+        size(800, 800);
+        
+    }
+    
+    public void setup() {
+        surface.setResizable(true);
+        frameRate(60);
+
+    }
+
+    public void draw() {
+        background(255, 0, 0);
+        //circle(mouseX, mouseY, 30);
+        //delay(100);
+        
+        
+        noFill();
+        beginShape();
+        for(int i = 0; i < n; i++) {
+            vertex(random(0, width), random(0, height));
+        }
+        endShape();
+
+    }
+
+    public static void main(String[] args) {
+        PApplet.main("Main");
+
+    }
+}
