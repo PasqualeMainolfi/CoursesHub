ec9b0185e73aa598c4897a492c4ecf386605fd53
PasqualeMainolfi <mnlpql@gmail.com>
2024-06-17 18:37:08 +0200

    les 17/06

--- LECTURE_NOTES/MSTPL/first_year_master/SensorECGAD8232/ad8232/ad8232.ino
+++ LECTURE_NOTES/MSTPL/first_year_master/SensorECGAD8232/ad8232/ad8232.ino
@@ -1,48 +1,79 @@
-// data in (ecg) -> text file (sd card) -> data elab.
-
+#include <SPI.h>
 #include <SD.h>
+#include <TimeLib.h>
 
-#define CHECK_SD (4)
+#define GSR_PLUS (8)
+#define GSR_MINUS (9)
 
-File data;
-bool run = true;
+File data_file;
 
-// aggiungere codice per la deallocazione di data (bottone per interrompere l'acquisizione)
-// gestire il nome file
-// funzione per la chiusura
+// pin select dell'SD card
+const int chipSelect = 10;
+
+int gsr_data;
+
+int btn_play_write = 2;
+int btn_stop_write = 3;
+int btn_clear = 4;
+
+bool write_sd = false;
+bool close_sd = false;
+
 
 void setup() {
   Serial.begin(9600);
 
-  if (!SD.begin(CHECK_SD)) {
-    Serial.println(F("SD CARD FAILED, OR NOT PRESENT!"));
-    while (1);
+  // differenza di potenziale GSR
+  pinMode(GSR_PLUS, INPUT); // Setup for leads off detection LO +
+  pinMode(GSR_MINUS, INPUT); // Setup for leads off detection LO -
+
+  pinMode(btn_play_write, INPUT_PULLUP);
+  pinMode(btn_stop_write, INPUT_PULLUP);
+  pinMode(btn_clear, INPUT_PULLUP);
+
+  Serial.print("Initializing SD card...");
+
+  if (!SD.begin()) {
+    Serial.println("initialization failed!");
+    return;
   }
 
-  if (!SD.exists("ecg-datalog_000.flb")) {
-    data = SD.open("ecg-datalog_000.flb", FILE_WRITE);
-  }
+  Serial.println("initialization done.");
 
-  pinMode(3, INPUT);
-  pinMode(2, INPUT);
+  time_t file_name = now();
+  
 
+  data_file = SD.open("DATA_GSR.txt", FILE_WRITE);
+  
 }
 
 void loop() {
 
-  int v1 = digitalRead(10);
-  int v2 = digitalRead(11);
+  if (data_file) {
 
-  if (run) {
-    if ((v1 == 1) || (v2 == 1)) {
-      Serial.println("!");
-    } else {
-      int val = analogRead(A5);
-      data.println(val);
-      Serial.println(val);
+    if (!digitalRead(btn_play_write)) {
+      write_sd = true;
     }
+
+    if (write_sd) {
+      data_file.println(gsr_data);
+      Serial.print("sto scrivendo: ");
+      Serial.println(gsr_data);
+    }
+
+    if(!digitalRead(btn_stop_write) && write_sd) {
+      data_file.close();
+      Serial.println("ho interrotto la scrittura!");
+      write_sd = false;
+    } 
+
+  }
+
+  if((digitalRead(GSR_PLUS) == 1) || (digitalRead(GSR_MINUS) == 1)) { // check if leads are removed
+    Serial.println("sensore non connesso ...");
   } else {
-    data.close();
+    gsr_data = analogRead(A0);  // valori di ampiezza della tensione di uscita dal sensore GSR
+    Serial.println(gsr_data);
   }
 
   delay(1);
